ed9b996c20f007c607853e3555824bbfc0dd7b08
Peter Ellis <peter.ellis2013nz@gmail.com>
2016-10-06 22:04:22 +1300

    initial commit

---
+++ .Rprofile
@@ -0,0 +1,7 @@
+ 
+
+try(source("P:/r/common.Rprofile"))
+
+
+
+
---
+++ prep/download-data.R
@@ -0,0 +1,5 @@
+
+download.file("http://robjhyndman.com/data/27-3-Athanasopoulos1.zip",
+              destfile = "source-data/27-3-Athansopoulos1.zip")
+?unzip
+unzip("source-data/27-3-Athansopoulos1.zip", )
