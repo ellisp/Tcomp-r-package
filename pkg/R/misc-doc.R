b893275f9a1d123f6a0053abe9edb3117b0dafa2
Peter Ellis <peter.ellis2013nz@gmail.com>
2016-10-09 15:25:12 +1300

    add a helpfile for Tcomp at package level

--- pkg/R/misc-doc.R
+++ pkg/R/misc-doc.R
@@ -26,3 +26,23 @@
 #' library(Mcomp) # for plot and print methods
 #' plot(tourism$Y1)
 "tourism"
+
+
+#' Tcomp: Data from the Tourism Forecasting competition
+#'
+#' The Tcomp package provides data from the tourism forecasting competition described
+#' in George. Athanasopolous, Rob J. Hyndman, Haiyan Song, Doris C. Wu (2011) The tourism forecasting competition, International Journal of Forecasting 27 (2011) 822-844.
+#' 
+#' 
+#' @section Tcomp data:
+#' \describe{
+#' \item{tourism}{A list of class Mdata with 1,311 time series in it, divided into training (x) and test (xx) sets. The data are subsettable with subset method from Mcomp R package by monthly, quarterly and yearly series.}
+#' }
+#' @section Tcomp functions:
+#' \describe{
+#' \item{forecast_comp}{A convenient wrapper function for providing the mean absolute scaled error (MASE) of four common forecasting methodologies for a time series of class Mdata.}
+#' }
+#'
+#' @docType package
+#' @name Tcomp
+NULL
